ae276099cfc4a3db54c4187387ea9f85e1c403a9
Kyaw Lynn Thant <kyawlinthant128380@gmail.com>
2025-07-31 13:09:32 +1000

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -225,7 +225,7 @@ export const Hero: React.FC<HeroProps> = ({ personalInfo, onUpdatePersonalInfo, 
             { label: 'About Me', target: 'about' },
             { label: 'Projects', target: 'projects' },
             { label: 'Work Exp', target: 'experience' },
-            { label: '💬', target: 'contact' }
+            { label: '⌯⌲', target: 'contact' }
           ].map((button, index) => (
             <motion.button
               key={button.target}
